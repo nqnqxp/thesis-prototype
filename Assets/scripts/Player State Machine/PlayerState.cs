514603ae12542ef790f77d82dbcbe9fed83a14e5
nqnqxp <124546324+nqnqxp@users.noreply.github.com>
2025-09-26 20:05:00 -0400

    saving

--- Assets/scripts/Player State Machine/PlayerState.cs
+++ Assets/scripts/Player State Machine/PlayerState.cs
@@ -9,12 +9,10 @@ public abstract class PlayerState
         this.stateMachine = stateMachine;
     }
 
-    // This method is called once when the state is entered.
+
     public abstract void EnterState();
 
-    // This method is called every frame to update the state's logic.
     public abstract void UpdateState();
 
-    // This method is called once when the state is exited.
     public abstract void ExitState();
 }
